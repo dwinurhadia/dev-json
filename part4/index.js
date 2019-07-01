fa642765759022b7fd4d0250c4ee88e2b7ac1208
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2019-07-01 11:30:42 +0700

    nambah parameter

--- part4/index.js
+++ part4/index.js
@@ -1,11 +1,12 @@
 var members = ['rizki','dodi','qaiserab'];
-
+// =================================== //
+// PART 2
 members.forEach(member => {
-    console.log(member);
+  //  console.log(member);
 });
 
 members.forEach((member,index) => {
-    console.log(member + '  '+ index);
+    //console.log(member + '  '+ index);
 });
 
 
@@ -14,10 +15,26 @@ let sekolahKoding = {
      ['rizki','dodi','quieser'],
     getMember(){
         this.members.map((name) => {
-            console.log(this);
+      //      console.log(this);
         });
     }
 };
 
 
-console.log(sekolahKoding.getMember());
+//console.log(sekolahKoding.getMember());
+// =================================== //
+// PART 3
+
+generateTitle = () => {
+    return 'Ini judul standar'
+}
+createTag = (title, tag) => {
+    tag = tag || 'CSS' //memberikan nilai default tag
+
+    if (typeof tag === 'undefined'){
+        tag = 'css'
+    }
+    console.log('Forum '+title+' punya tag '+tag)
+}
+
+createTag('tampilan rusak');
